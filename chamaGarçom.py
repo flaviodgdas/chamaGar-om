8bcdca168099bd3def03e4afb7a50deb95343891
Flávio Domingos Gomes da Silva <161535510+flaviodgdas@users.noreply.github.com>
2025-03-31 19:00:14 -0300

    Update chamaGarçom.py
    atualiza

--- chamaGarçom.py
+++ chamaGarçom.py
@@ -5,7 +5,7 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
-
+# Chama o garçom
 class MesaApp(App):
     def build(self):
         self.layout = BoxLayout(orientation='vertical')
